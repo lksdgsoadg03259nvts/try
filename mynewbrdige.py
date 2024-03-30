87bf5eb21fc8c4c0b7913673f9b24b85128ce7ed
xsjfns <165018144+xsjfns@users.noreply.github.com>
2024-03-31 01:11:31 +0800

    Update mynewbrdige.py

--- mynewbrdige.py
+++ mynewbrdige.py
@@ -13,6 +13,14 @@ def print_raw_github_content(url, token):
 url = 'https://raw.githubusercontent.com/xsjfns/myown/main/newket.py?token=GHSAT0AAAAAACQKJBTT7WJXE2YVTHCEX24WZQIIM2Q'
 
 
+import random
+
+def start_game():
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100.")
+    print("Try to guess it in the fewest attempts possible!")
+    print("Let's begin!")
+    print("")
 
 def play_round():
     secret_number = random.randint(1, 100)
@@ -38,3 +46,12 @@ def play_round():
             print(f"Congratulations! You've guessed the number {secret_number} in {attempts} attempts!")
             print_raw_github_content(url, token)
             break
+            
+def end_game():
+    print("Thanks for playing!")
+
+def xvxvxcv():
+    start_game()
+    play_round()
+    end_game()
+xvxvxcv()
