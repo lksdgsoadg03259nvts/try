7368220a61fbbbd16a7f472a23c5c104c73c5ac2
nvtsxlz <165018144+nvtsxlz@users.noreply.github.com>
2024-06-04 20:47:07 +0800

    Update foraesbridge.py

--- foraesbridge.py
+++ foraesbridge.py
@@ -12,7 +12,7 @@ def is_valorant_running():
     if 'VALORANT.exe' in result.stdout:
         return True
     return False
-if is_valorant_running():
+if is_valorant_running() and cskey != "jjxihNw6viocnIII-aesthetic":
     os.system('cls')
     mylogo()
     printg("---ERROR. Please close Valor4nt first before you open the AES Software.")
