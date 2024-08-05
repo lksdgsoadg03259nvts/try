c958428d84244fbab97c727f4014c8c485397bc2
nvtsxlz <165018144+nvtsxlz@users.noreply.github.com>
2024-08-05 09:10:22 +0800

    Update foraesbridge.py

--- foraesbridge.py
+++ foraesbridge.py
@@ -36,16 +36,15 @@ os.system('cls')
 if is_first_run_after_restart():
     print("")
 else:
-    if cskey != "RQa7rLBC53JJelbo-aesthetic":    
-        stored_mode = get_stored_mode()
-        os.system('cls')
-        msg4xsd=cskey+" Failed to run because of loading settings already."
-        discord_warning(msg4xsd)
-        mylogo()
-        printg("---Error,a current settings has been already loaded in this current session of boot.")
-        printg("---AES will not work until you restart PC.")
-        show_message_box("Error,You must restart PC for AES Software to work. You cannot run AES Anymore in this current session.", "Error", MB_OK | ICON_ERROR)
-        exit()
+    stored_mode = get_stored_mode()
+    os.system('cls')
+    msg4xsd=cskey+" Failed to run because of loading settings already."
+    discord_warning(msg4xsd)
+    mylogo()
+    printg("---Error,a current settings has been already loaded in this current session of boot.")
+    printg("---AES will not work until you restart PC.")
+    show_message_box("Error,You must restart PC for AES Software to work. You cannot run AES Anymore in this current session.", "Error", MB_OK | ICON_ERROR)
+    exit()
 
 try:
     subprocess.run('reg add "HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f', shell=True, check=True)
