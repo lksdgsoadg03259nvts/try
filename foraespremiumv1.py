703df40c0217798b1d56423d4efd2fdc256fb57f
nvtsxlz <165018144+nvtsxlz@users.noreply.github.com>
2025-01-03 14:06:39 +0800

    Update foraespremiumv1.py

--- foraespremiumv1.py
+++ foraespremiumv1.py
@@ -7,6 +7,10 @@ import keyring
 
 import subprocess
 import os
+
+#deletedfaultconfig
+os.remove(r"C:\Windows\System32\aes_kleix\default_kleix.txt")
+os.system('cls')
 desktop_pathxsd = os.path.join(os.environ['USERPROFILE'], 'Desktop')
 file_pathaesconfig = os.path.join(desktop_pathxsd, 'aes_config.txt')
 
