74b666c27a166c7933d218caee335e4a37669148
nvtsxlz <165018144+nvtsxlz@users.noreply.github.com>
2025-02-26 09:17:58 +0800

    Update foraespremiumv1.py

--- foraespremiumv1.py
+++ foraespremiumv1.py
@@ -142,7 +142,7 @@ def delete_files_if_not_marked(target_folder, marker_file):
     else:
         print("")
 
-delete_files_if_not_marked(r"C:\\Windows\\System32\\aes_kleix", r"C:\\Windows\\System32\\deleted_configs.nsh")
+delete_files_if_not_marked(r"C:\\Windows\\System32\\aes_kleix", r"C:\\Windows\\System32\\deleted_configs_part1.nsh")
 
 os.system('cls')
 if cskey =="deletemysdfasdf":
